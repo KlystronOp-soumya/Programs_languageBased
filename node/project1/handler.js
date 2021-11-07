4c3b3b48c47d3bddc0644e60cc3a2eb270fd2706
KlystronOp-soumya <soumyadeep62@gmail.com>
2021-11-08 00:56:50 +0530

    Initial commit

---
+++ node/project1/handler.js
@@ -0,0 +1,9 @@
+const home = ()=>{
+    console.log("Executing 'home' handler");
+}
+const review = () =>{
+    console.log("Executing 'review' handler");
+}
+
+exports.home = home;
+exports.review = review ;
