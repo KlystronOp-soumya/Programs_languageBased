14af6a1af28536d9f2141ac90d3584cb4852b368
KlystronOp-soumya <soumyadeep62@gmail.com>
2021-11-08 00:55:17 +0530

    Includes handler module

--- node/project1/app.js
+++ node/project1/app.js
@@ -1,3 +1,10 @@
 let server = require("./server")
 let route = require("./route")
-server.startServer(route.router);
+let handler = require("./handler");
+
+handle={};
+handle["/home"] = handler.home;//stores the function
+handle["/review"] = handler.review ;
+
+
+server.startServer(route.router,handle);
