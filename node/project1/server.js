83dd0af7557b223225b9d5566a8bb1c837cb2608
KlystronOp-soumya <soumyadeep62@gmail.com>
2021-11-08 00:35:27 +0530

    Includes route function

--- node/project1/server.js
+++ node/project1/server.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const url = require('url');
 
-const startServer = () => {
+const startServer = (router) => { //router is the function
   const onRequest =  (request,response) => {
       console.log("Received request");
+      var pathName = url.parse(request.url).pathname ;
+      console.log("Request received for "+pathName);
+      router(pathName);
       response.writeHead(200, "Server Up", { "content-type": "text/plain" });
       response.write("Hello From Server");
       response.end();
@@ -11,4 +15,4 @@ const startServer = () => {
 http.createServer(onRequest).listen(8000) ;
 console.log("Server running at port:8000");
 }
-exports.startServer = startServer
+exports.startServer = startServer ;
