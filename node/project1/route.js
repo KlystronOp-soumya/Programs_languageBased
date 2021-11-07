f9ca36066df7bb2cc10906ab7b1afa39e8ab0f56
KlystronOp-soumya <soumyadeep62@gmail.com>
2021-11-08 00:55:49 +0530

    Includes handler module

--- node/project1/route.js
+++ node/project1/route.js
@@ -1,4 +1,12 @@
-const router = (pathName)=>{
- console.log("Routing a request for "+pathName);
+const router = (pathName,handle)=>{
+ //console.log("Routing a request for "+pathName);
+ if(typeof handle[pathName] === 'function')
+ {
+    handle[pathName]();
+ }
+ else
+ {
+    console.error("no handler for the path: "+pathName);
+ }
 }
 module.exports.router = router ;
--- node/project1/server.js
+++ node/project1/server.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const url = require('url');
 
-const startServer = (router) => { //router is the function
+const startServer = (router,handle) => { //router is the function
   const onRequest =  (request,response) => {
       console.log("Received request");
       var pathName = url.parse(request.url).pathname ;
       console.log("Request received for "+pathName);
-      router(pathName);
+      router(pathName,handle);
       response.writeHead(200, "Server Up", { "content-type": "text/plain" });
       response.write("Hello From Server");
       response.end();
