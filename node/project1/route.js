17dceaf2e279887ed335df9c58f1cbff4f142b77
KlystronOp-soumya <soumyadeep62@gmail.com>
2021-11-08 01:50:02 +0530

    Modifies route with response parameter

--- node/project1/route.js
+++ node/project1/route.js
@@ -1,12 +1,15 @@
-const router = (pathName,handle)=>{
+const router = (pathName,handle,response)=>{
  //console.log("Routing a request for "+pathName);
  if(typeof handle[pathName] === 'function')
  {
-    handle[pathName]();
+    handle[pathName](response);
  }
  else
  {
     console.error("no handler for the path: "+pathName);
+    response.writeHead(404,{"content-type": "text/plain" });
+      response.write("404 Not Found");
+      response.end();
  }
 }
 module.exports.router = router ;
