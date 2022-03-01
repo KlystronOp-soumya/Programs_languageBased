a73ca87cddb63153f651d6426dad4bcf3d13501a
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-03-02 01:06:52 +0530

    Modifies the handler for registration

--- node/Express Demo/app.js
+++ node/Express Demo/app.js
@@ -1,6 +1,4 @@
-const { response } = require('express');
 const express = require('express');
-const { request } = require('http');
 var bodyParser = require("body-parser");
 
 var app = express();
@@ -11,18 +9,30 @@ app.use(express.static(__dirname + "/login.html"));
 
 app.get("/",(request,response) =>{
     response.send("Hello World");
+    //usign the request parametes to extract different informations
+
     
 });
 app.get("/login",(request,response)=>{
     response.sendFile(__dirname + "/login.html");
 });
 
-app.post("/validateCreds",(request,response)=>{
+app.get("/register",(request,response)=>{
+    request.accepts(["html"]);
+    response.sendFile(__dirname+"/register.html");
+});
+
+app.post("/doRegister/:name",(request,response)=>{
+    console.log(request.params)
+    response.status(200).send("<h4>Name is : </h4>"+request.params.name) ;
+});
+
+app.post("/validateCreds/",(request,response)=>{
     console.log(`Received User ID: ${request.body.userID} Password: ${request.body.Password}`) ;
     response.status(200).send("<h4>Received</h4>") ;
 });
 
-var server=app.listen(3000,()=>{
+var server=  app.listen(3000,()=>{
     console.log("Listening to the port 3000");
     var host=server.address();//returns a json
     var port=server.address().port;
