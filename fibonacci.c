14b24ed71d79f265169fec583173d2518af084c2
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-03-05 11:56:11 +0530

    Implemets recursive approach

--- fibonacci.c
+++ fibonacci.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 
+int fib (int n)
+{
+    if(n==0)
+     return 0;
+    if(n == 1)
+    return 1 ;
+
+    return fib(n-1)+fib(n-2);
+}
+
 int main() {
+
+printf("%d",fib(8)) ;
 
 return 0;
 }
