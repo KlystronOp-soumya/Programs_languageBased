7164fd90be576610ee596c40be1210b7a2f1efcd
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-03-06 11:43:15 +0530

    Implements strrev

---
+++ string_rev.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+#define MAX_ELEM 100
+ 
+
+ char *strrev_impl(char *str)
+ {
+     return NULL ;
+ }
+
+ 
+int main(int argc,char*argv[])
+{
+ char str [] ="hello" ;
+ printf("Reversed string: %s",strrev(str)); //lib function
+return 0;
+}
