5d0705f924c8995ea75a899023fa29f2d5877b6a
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-03-08 01:31:14 +0530

    Adds other datatypes

--- Demos/DataTypes.clj
+++ Demos/DataTypes.clj
@@ -8,12 +8,16 @@
   (def y 2)
   (def greet "Hello World")
   (def status true)
+  ;;(def m Nil) ;;The Nil is equivalent to NULL
+  (def n (atom 1))
   (def z (not= x y))
   (println x)
   (println y)
   (println greet)
   (println status)
   (println z)
+  ;;(println m)
+  (println n)
   )
  
 
