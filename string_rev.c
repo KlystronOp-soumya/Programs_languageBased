fa9979f19d6fd7ab16c2fd4126238bbebc4e26cc
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-03-12 11:10:32 +0530

    Implements String Reverse-Initial commit

--- string_rev.c
+++ string_rev.c
@@ -8,13 +8,30 @@
 
  char *strrev_impl(char *str)
  {
-     return NULL ;
+     char *rev_str = NULL ;
+    //dynamic memory allocation
+    rev_str = (char *)malloc(sizeof(char)*strlen(str));
+    int i ,j = 0;
+    for(i = strlen(str)-1 ; i>=0 ;i--,j++ )
+    {
+        
+        rev_str[j] = str[i] ;
+        //printf("Character scanned: %c\n",rev_str[j]) ;
+    }
+    j++;
+    rev_str[j]='\0' ;
+    
+    return rev_str ;
+
  }
 
  
 int main(int argc,char*argv[])
 {
- char str [] ="hello" ;
- printf("Reversed string: %s",strrev(str)); //lib function
+ char str [MAX_ELEM]  ;
+ printf("Enter a String: ");
+ scanf("%[^\n]s",str);
+ //printf("Reversed string: %s",strrev(str)); //lib function
+ printf("Rev: %s ",strrev_impl(str));
 return 0;
 }
