82fed619839bc93180fd1038a9d6b9bccb1bb4c7
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-03-14 21:35:49 +0530

    Adds another function

--- string_concat.c
+++ string_concat.c
@@ -31,15 +31,39 @@
 
  }
  
+ void xstringcat(char *target,char *source)
+ {
+     int i;  
+     target=(char*)malloc(sizeof(char)*5) ;
+     printf("Enter string: ") ;
+    fflush(stdin);
+    gets(target);
+    source=(char*)malloc(sizeof(char)*1);
+     printf("Enter another string: ");
+     scanf("%[^\n]s",source);
+     printf("You have entered: \"%s\" and \"%s\" \n",target,source) ;
+    
+    //realloc(target,strlen(source));
+
+    for( i =0; target[i]!='\0';i++); //getting the last pos
+
+    for(int j =0;j<strlen(source);j++,i++)
+    {
+        target[i] = source[j];
+    }
+    target[i]='\0'; //terminating
+    printf("After concatenation: %s ",target) ;
+ }
 int main(int argc,char*argv[])
 {
  //strcat
     char str1[]="hello" ;
     char str2[7]="_world" ;
 
+    char*t=NULL,*s=NULL;
     //strcat(str1,str2);
-    strcat_impl(str1,str2) ;
-
+   // strcat_impl(str1,str2) ;
+     xstringcat(t,s);
     printf("Concatenated (impl): %s",str1) ;
 
 return 0;
