256923127955490408fe53d1a2f8f6ffd24cbea3
KlystronOp-soumya <soumyadeep62@gmail.com>
2023-07-07 00:04:31 +0530

    Adds new Class script

---
+++ src/main/groovy/Book.groovy
@@ -0,0 +1,19 @@
+/* Simple class written in Groovy
+Dont put any public keyword like Java */
+
+class Book
+{
+ //attribute
+ private String title 
+
+ Book(String theTitle)
+ {
+    title = theTitle
+ }
+
+ String getTitle()
+ {
+    return title
+ }
+
+}
