e94c646a4e95987464eb6b8c0a63289e5532430e
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-04-19 00:30:07 +0530

    Adds processor class for thread control

---
+++ src/com/demo/threading/Processor.java
@@ -0,0 +1,69 @@
+package com.demo.threading;
+
+import java.util.Scanner;
+
+public class Processor {
+
+	public void producer() throws InterruptedException {
+
+		Thread.currentThread().setName("Producer thread");
+		synchronized (this) {
+			Thread.sleep(2000);
+			System.out.println("Thread " + Thread.currentThread().toString() + " was put to sleep before wait");
+			wait();
+		}
+		System.out.println("Producer was resumed");
+
+	}
+
+	public void consumer() throws InterruptedException {
+
+		final Scanner sc = new Scanner(System.in);
+		Thread.currentThread().setName("Consumer Thread");
+		Thread.sleep(3000);
+		synchronized (this) {
+			System.out.println("Waiting for return key...");
+			sc.nextLine();
+			System.out.println("Return key was pressed!");
+			notify();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Processor prod = new Processor();
+		Processor cons = new Processor();
+		try {
+			Runnable prodRun = () -> {
+				try {
+
+					prod.producer();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			};
+
+			Runnable consRun = () -> {
+				try {
+					prod.consumer();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			};
+
+			Thread t1 = new Thread(prodRun);
+			Thread t2 = new Thread(consRun);
+
+			t1.start();
+			t2.start();
+
+			t1.join();
+			t2.join();
+
+		} catch (Exception e) {
+
+		}
+	}
+}
