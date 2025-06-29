55753313ff3b3e12a06da04919dc294038030e16
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-06-29 16:44:58 +0530

    strings in go

--- strings.go
+++ strings.go
@@ -5,7 +5,15 @@ import "fmt"
 func strings() {
 	//strings in go lang
 	//declare strings
-	
-	fmt.Println()
+    var firstName string = "John Doe" ;
+	fmt.Println(firstName)
+	//with escape modifiers
+	address:= "The Whitre House\n1600 Pennsylvania Ave.\n WDC"
+	fmt.Println(address)
+	//raw string
+	rawStr:=`
+			"this is a raw string"
+	` ;
+	println(rawStr);
 
 }
