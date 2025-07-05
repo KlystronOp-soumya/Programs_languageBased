d2984af33841cd87f9c154661fc226a45be98858
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-07-05 16:54:59 +0530

    adds Info method

--- loggerutil.go
+++ loggerutil.go
@@ -1,8 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
 // LogValue logs any type of value using Go generics
 func LogValue[T any](label string, value T) {
 	log.Printf("%s: %+v\n", label, value)
+}
+func Info[T any]( msgLabel string, value ... T)  {
+	
+	for _, test := range value {
+		log.Println(msgLabel,":: " , test) ;
+	}
 }
