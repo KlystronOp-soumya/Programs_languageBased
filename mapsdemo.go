269361b27db6db000a84ee4f03c50ef28cc2546a
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-07-05 16:55:13 +0530

    adds map iteration

--- mapsdemo.go
+++ mapsdemo.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 /*
   Map<String , Integer> - Java
   map<String, int> - cpp
@@ -16,6 +21,34 @@ func mapsdemo() {
 
 	LogValue("heights" , heights) ;
 
+	//initialize map with the map literal
+	marks := map[string]float32{
+		"John Doe" : 99.9 ,
+		"Jane Max" : 100.0 , //end with an extra comma
+	} ;
+	LogValue("marks" , marks) ;
+	//delete a key in map
+	delete(marks , "Jane Max");
 
+	if v,ok := heights["Peter"]; ok{
+		Info("Found" , v , 1) ;
+	}
+
+	fmt.Println(marks);
+	fmt.Println("Length of the map: " , len(heights))
+
+	//iterate
+	for k,v := range heights {
+		fmt.Println(k ,"->" , v);
+	}
+	//get all the keys in map
+	var keys []string ;
+	for k := range heights {
+		keys =append(keys, k) ;
+	}
+	fmt.Println(keys)
+	//sort the keys
+	sort.Strings(keys)
+	fmt.Println("after sorting", keys) ;
 
 }
