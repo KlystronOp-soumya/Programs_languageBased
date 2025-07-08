a5a45d46ba0cadd1db79c838267e32bea92fc891
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-07-08 23:32:56 +0530

    adds function

--- main.go
+++ main.go
@@ -5,40 +5,40 @@ import (
 )
 
 /*
- to execute this code
- go build .\main.go
- .\main.exe
+to execute this code
+go build .\main.go
+.\main.exe
 
- or, go run .\main.go
+or, go run .\main.go
 
- now we need to compile both the files main.go and show_time.go
- then use go build
+now we need to compile both the files main.go and show_time.go
+then use go build
 */
 var globalNum int //explicitly defined
-func main(){
+func main() {
 	fmt.Println("Hello World")
 
 	//call the displayTime from show_time
-	displayTime();
+	displayTime()
 
 	//declare variable
-	var num1 = 5;
-	fmt.Println("The number is:",num1)
+	var num1 = 5
+	fmt.Println("The number is:", num1)
 
-	fmt.Println("This is global:",globalNum)
-	globalNum=13
-	fmt.Println("Assigned value into global:" , globalNum);
+	fmt.Println("This is global:", globalNum)
+	globalNum = 13
+	fmt.Println("Assigned value into global:", globalNum)
 
 	//define different types of variables
-	var num2 int64 ;
-	var num3 float32 ;
-	var raining bool ;
-	fmt.Println(num3);
-	fmt.Println(num2);
-	fmt.Println(raining);
+	var num2 int64
+	var num3 float32
+	var raining bool
+	fmt.Println(num3)
+	fmt.Println(num2)
+	fmt.Println(raining)
 
-	var rates float32 = 4.5 ;
-	fmt.Println(rates);
+	var rates float32 = 4.5
+	fmt.Println(rates)
 
 	fmt.Println("---------Variables demo start--------")
 	variables()
@@ -52,11 +52,12 @@ func main(){
 	fmt.Println("------------TypeConversion demo end-----------")
 
 	//mymain() //uncomment for user input
-	parsing();
-	makedecesion();
+	parsing()
+	makedecesion()
 	//loopDemo() ;
-	funcdemo();
-	funcAdvance();
-	structdemo() ;
-	mapsdemo();
+	funcdemo()
+	funcAdvance()
+	structdemo()
+	mapsdemo()
+	interfaces()
 }
