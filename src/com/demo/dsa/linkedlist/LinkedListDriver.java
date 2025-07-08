4b1d4266ad7dfdaee780d373f430d713b2c90ed5
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-07-09 01:58:03 +0530

    Creates classes for LinkedList ADT

---
+++ src/com/demo/dsa/linkedlist/LinkedListDriver.java
@@ -0,0 +1,10 @@
+package com.demo.dsa.linkedlist;
+
+public class LinkedListDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/demo/dsa/linkedlist/SinglyLLNode.java
@@ -0,0 +1,5 @@
+package com.demo.dsa.linkedlist;
+
+public class SinglyLLNode {
+
+}
