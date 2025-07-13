7bfabe6562fef59958146f37eaf2881c0eb13561
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-07-13 23:38:58 +0530

    Box class

---
+++ box.cpp
@@ -0,0 +1,27 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define END_LINE '\n'
+
+class Box
+{
+private:
+    double h, w, b;
+
+public:
+    Box() {}
+    ~Box() {}
+
+    Box(const double height, const double width, const double breadth)
+    {
+        this->h = height;
+        this->b = breadth;
+        this->w = width;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
