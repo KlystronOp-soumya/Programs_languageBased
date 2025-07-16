a915e93da19b71794be2fe71252c5a6d842d9c1a
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-07-16 15:22:54 +0530

    Box.cpp

--- box.cpp
+++ box.cpp
@@ -3,11 +3,19 @@
 using namespace std;
 #define END_LINE '\n'
 
-class Box
+class Shape
+{
+
+public:
+    double h, w, b;
+    double area(double h, double b);
+    double area(double h, double w, double b);
+    double area(double s);
+};
+
+class Box : public Shape // extends Shape
 {
 private:
-    double h, w, b;
-
 public:
     Box() {}
     ~Box() {}
@@ -17,6 +25,10 @@ public:
         this->h = height;
         this->b = breadth;
         this->w = width;
+    }
+
+    double area()
+    {
     }
 };
 
