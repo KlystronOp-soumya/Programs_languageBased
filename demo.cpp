fa1737b2d8b616511915c9bfb26fb517d1cb0afe
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-08-01 01:09:25 +0530

    assert in cpp

--- demo.cpp
+++ demo.cpp
@@ -1,27 +1,103 @@
 #pragma GCC optimize("O3,unroll-loops")
 #include <bits/stdc++.h>
+//#include "constants.h"
+#include "constinline.h"
 // #include <print>
 // #include <fmt/core.h>
 
-#include <format>
-using namespace std;
-constexpr char END_LINE {'\n'} ;
-#define NAME "John"
-
-//A bit mask is a predefined set of bits that is used to select which specific bits will be modified by subsequent operations.
-void bitmask(){
-
+/*
+    Use of constexpr in function
+*/
+constexpr double calcCircumference(double radius) // now a constexpr function
+{
+    constexpr double pi { 3.14159265359 };
+    return 2.0 * pi * radius;
 }
 
-void bitWiseBitset(){
-    //Bit-shifting in C++ is endian-agnostic. Left-shift is always towards the most significant bit, and right-shift towards the least significant bit.
-    bitset<4> b {0b1100 } ;
-    cout<< b << END_LINE ;
-    cout << (b >> 1) << END_LINE ; //shift 1 yields 0110
-    cout << (b<<1) << END_LINE ; //shift left by 1 yields 1000
+void useConstExpr(){
+     constexpr double circumference { calcCircumference(3.0) }; // now compiles -- because we have defined the function with constexpr; had we not done so, we would have never compiled it successfully
+     cout<< "Circum of the circle:" << circumference << END_LINE;
+}
 
-     std::bitset<4> b4{ 0b100 }; // b4 is 0100
-    std::bitset<8> b8{ 0b100 }; // b8 is 0000 0100
+/*
+    type alias
+*/
+
+static void typeAliases(){
+    using Distance = double; // define Distance as an alias for type double
+
+    Distance milesToDestination{ 3.4 }; // defines a variable of type double
+
+    std::cout << milesToDestination << '\n'; // prints a double value
+}
+
+//assertion at compile time
+ // static_assert(sizeof(long) >= 8, "long must be 8 bytes");
+static_assert(sizeof(int) >= 4, "int must be at least 4 bytes");
+
+void assertion(){
+    // #define NDEBUG // disable asserts (must be placed before any #includes)
+    bool test = false ;
+    assert(!test && "test failed") ;
+}
+
+
+/*
+Only use extern for global variable forward declarations or const global variable definitions.
+Do not use extern for non-const global variable definitions (they are implicitly extern).
+*/
+
+int val = 5 ; 
+
+void useConstantsWithOutInline(){
+    /*
+    Advantages:
+
+Works prior to C++17.
+Only one copy of each variable is required.
+Only requires recompilation of one file if the value of a constant changes.
+Disadvantages:
+
+Forward declarations and variable definitions are in separate files, and must be kept in sync.
+Variables not usable in constant expressions outside of the file in which they are defined.
+    
+    */
+    std::cout << "Enter a radius: ";
+    double radius{};
+    std::cin >> radius;
+    //using namespace constinline ;
+    std::cout << "The circumference is: " << 2 * radius * constinline::pi << '\n';
+}
+
+
+
+void globlaVarShadow(){
+     int val = 100 ;
+     cout << val++ << " local" << END_LINE;
+     ++(::val) ; //avoid variable shadowing and use g_ prefix for globals
+     cout << val << "global" << END_LINE ;
+}
+
+#include <format>
+using namespace std;
+constexpr char END_LINE{'\n'};
+#define NAME "John"
+
+// A bit mask is a predefined set of bits that is used to select which specific bits will be modified by subsequent operations.
+void bitmask()
+{
+}
+
+void bitWiseBitset()
+{
+    // Bit-shifting in C++ is endian-agnostic. Left-shift is always towards the most significant bit, and right-shift towards the least significant bit.
+    bitset<4> b{0b1100};
+    cout << b << END_LINE;
+    cout << (b >> 1) << END_LINE; // shift 1 yields 0110
+    cout << (b << 1) << END_LINE; // shift left by 1 yields 1000
+
+    std::bitset<4> b4{0b100}; // b4 is 0100
+    std::bitset<8> b8{0b100}; // b8 is 0000 0100
 
     std::cout << "Initial values:\n";
     std::cout << "Bits: " << b4 << ' ' << b8 << '\n';
@@ -32,42 +108,41 @@ void bitWiseBitset(){
 
     std::cout << "After bitwise NOT:\n";
     std::cout << "Bits: " << b4 << ' ' << b8 << '\n';
-    std::cout << "Values: " << b4.to_ulong() << ' ' << b8.to_ulong() << '\n'; //converts to long
-
-
+    std::cout << "Values: " << b4.to_ulong() << ' ' << b8.to_ulong() << '\n'; // converts to long
 }
 
-void bitsets(){
+void bitsets()
+{
 
-    bitset<8> bits { 0b000'0101 } ;
-    bits.set(3) ; //set bit position 3 to 1
-    bits.flip(4) ; // toggle the bit at position 4 from right idx =1 
-    bits.reset(4) ; //sets back to 0
+    bitset<8> bits{0b000'0101};
+    bits.set(3);   // set bit position 3 to 1
+    bits.flip(4);  // toggle the bit at position 4 from right idx =1
+    bits.reset(4); // sets back to 0
 
-    cout << "All the bits" << bits << END_LINE ;
-    cout << "Bit has 3 value" << bits.test(3) << END_LINE ;
-    cout << "Bit 4 has value" << bits.test(4) << END_LINE ;
+    cout << "All the bits" << bits << END_LINE;
+    cout << "Bit has 3 value" << bits.test(3) << END_LINE;
+    cout << "Bit 4 has value" << bits.test(4) << END_LINE;
 
-    //better use enums
-    [[maybe_unused]] constexpr int  isHungry   { 0 };
-    [[maybe_unused]] constexpr int  isSad      { 1 };
-    [[maybe_unused]] constexpr int  isMad      { 2 };
-    [[maybe_unused]] constexpr int  isHappy    { 3 };
-    [[maybe_unused]] constexpr int  isLaughing { 4 };
-    [[maybe_unused]] constexpr int  isAsleep   { 5 };
-    [[maybe_unused]] constexpr int  isDead     { 6 };
-    [[maybe_unused]] constexpr int  isCrying   { 7 };
+    // better use enums
+    [[maybe_unused]] constexpr int isHungry{0};
+    [[maybe_unused]] constexpr int isSad{1};
+    [[maybe_unused]] constexpr int isMad{2};
+    [[maybe_unused]] constexpr int isHappy{3};
+    [[maybe_unused]] constexpr int isLaughing{4};
+    [[maybe_unused]] constexpr int isAsleep{5};
+    [[maybe_unused]] constexpr int isDead{6};
+    [[maybe_unused]] constexpr int isCrying{7};
 
-    std::bitset<8> me{ 0b0000'0101 }; // we need 8 bits, start with bit pattern 0000 0101
-    me.set(isHappy);      // set bit position 3 to 1 (now we have 0000 1101)
-    me.flip(isLaughing);  // flip bit 4 (now we have 0001 1101)
-    me.reset(isLaughing); // set bit 4 back to 0 (now we have 0000 1101)
+    std::bitset<8> me{0b0000'0101}; // we need 8 bits, start with bit pattern 0000 0101
+    me.set(isHappy);                // set bit position 3 to 1 (now we have 0000 1101)
+    me.flip(isLaughing);            // flip bit 4 (now we have 0001 1101)
+    me.reset(isLaughing);           // set bit 4 back to 0 (now we have 0000 1101)
 
     std::cout << "All the bits: " << me << '\n';
     std::cout << "I am happy: " << me.test(isHappy) << '\n';
     std::cout << "I am laughing: " << me.test(isLaughing) << '\n';
 
-    //Querying bitset
+    // Querying bitset
     std::cout << bits.size() << " bits are in the bitset\n";
     std::cout << bits.count() << " bits are set to true\n";
 
@@ -76,44 +151,45 @@ void bitsets(){
     std::cout << "Some bits are true: " << bits.any() << '\n';
     std::cout << "No bits are true: " << bits.none() << '\n';
 }
-void printStr(string_view str){
-    cout<< str << END_LINE ;
-    //get a mutable string
-    string st {str} ; //ok
-    //string st1 = str ; //not ok
-    string st2 ;
-    st2 = static_cast<string> (str) ; //ok
-    reverse(st.begin() , st.end()) ;
+void printStr(string_view str)
+{
+    cout << str << END_LINE;
+    // get a mutable string
+    string st{str}; // ok
+    // string st1 = str ; //not ok
+    string st2;
+    st2 = static_cast<string>(str); // ok
+    reverse(st.begin(), st.end());
 }
 
-void stringView(){
-    std::string str {"Hello World"} ; // is an expensive operation
+void stringView()
+{
+    std::string str{"Hello World"}; // is an expensive operation
     // any function call with this str maintains 2 copies of str better use string_view that only passes a read only single copy
-    printStr(str) ;
-
-
+    printStr(str);
 }
 
-void stringView(){
+void stringView()
+{
 
-     using namespace std::string_literals;      // access the s suffix
+    using namespace std::string_literals;      // access the s suffix
     using namespace std::string_view_literals; // access the sv suffix
 
     std::cout << "foo\n";   // no suffix is a C-style string literal
     std::cout << "goo\n"s;  // s suffix is a std::string literal
     std::cout << "moo\n"sv; // sv suffix is a std::string_view literal
 
-    std::string name { "Alex" };
-    std::string_view sv { name }; // sv is now viewing name
-    std::cout << sv << '\n'; // prints Alex
+    std::string name{"Alex"};
+    std::string_view sv{name}; // sv is now viewing name
+    std::cout << sv << '\n';   // prints Alex
 
-    sv = "John"; // sv is now viewing "John" (does not change name)
+    sv = "John";             // sv is now viewing "John" (does not change name)
     std::cout << sv << '\n'; // prints John
 
     std::cout << name << '\n'; // prints Alex
 
-    constexpr std::string_view s{ "Hello, world!" }; // s is a string symbolic constant
-    std::cout << s << '\n'; // s will be replaced with "Hello, world!" at compile-time 
+    constexpr std::string_view s{"Hello, world!"}; // s is a string symbolic constant
+    std::cout << s << '\n';                        // s will be replaced with "Hello, world!" at compile-time
 }
 void strings()
 {
@@ -124,19 +200,18 @@ void strings()
 
     std::cout << "Now enter your name: ";
     std::string name{};
-    //ws is the input string manipulator that discards any leading space
+    // ws is the input string manipulator that discards any leading space
     std::getline(std::cin >> std::ws, name); // note: added std::ws here
 
     std::cout << "Hello, " << name << ", you picked " << choice << '\n';
 
-     using namespace std::string_literals; // easy access to the s suffix
+    using namespace std::string_literals; // easy access to the s suffix
 
-    std::cout << "foo\n";   // no suffix is a C-style string literal with Null termination
-    std::cout << "goo\n"s;  // s suffix is a std::string literal withotu null termination
+    std::cout << "foo\n";  // no suffix is a C-style string literal with Null termination
+    std::cout << "goo\n"s; // s suffix is a std::string literal withotu null termination
     /*
     constexpr std::string name{ "Alex"s }; // compile error
     */
- 
 }
 
 void literals()
