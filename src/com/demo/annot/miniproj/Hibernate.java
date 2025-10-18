4d81549e2178dcfc23be09536e8225deabdda31d
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-10-18 18:09:43 +0530

    Generates sql string with Placeholders

--- src/com/demo/annot/miniproj/Hibernate.java
+++ src/com/demo/annot/miniproj/Hibernate.java
@@ -1,9 +1,11 @@
 package com.demo.annot.miniproj;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Hibernate<T> {
 
@@ -17,10 +19,11 @@ public class Hibernate<T> {
 	public void save(T entity) throws Exception {
 		Class<T> clazz = (Class<T>) entity.getClass();
 		// get the table name
-		Annotation annotation;
+		ToTable annotation;
 		if (clazz.isAnnotationPresent(ToTable.class)) {
 			annotation = clazz.getAnnotation(ToTable.class);
-			this.table = ((ToTable) annotation).name();
+			this.table = annotation.name().isBlank() ? clazz.getName() : annotation.name();
+			System.out.println("Provided table name: " + this.table);
 		}
 		/*
 		 * Annotation[] classAnnotations = clazz.getAnnotations(); for (Annotation
@@ -30,20 +33,45 @@ public class Hibernate<T> {
 		 */
 
 		// get the columns
+		HashMap<Field, Class<?>> columnNameTypeMap = new HashMap<>();
+
 		Field[] fields = entity.getClass().getDeclaredFields();
+
 		for (Field eachField : fields) {
+
 			if (eachField.isAnnotationPresent(PrimaryKey.class)) {
 				PrimaryKey primaryKey = eachField.getAnnotation(PrimaryKey.class);
+
 				System.out.println(primaryKey.name() + ": " + primaryKey.autoIncrement());
+				columnNameTypeMap.put(eachField, eachField.getType());
 			} else if (eachField.isAnnotationPresent(Column.class)) {
+
 				Column columnAnnotation = eachField.getAnnotation(Column.class);
+
 				if (columnAnnotation.name().isBlank()) {
 					System.out.println(eachField.getName().toUpperCase());
 				} else {
 					System.out.println(columnAnnotation.name());
 				}
+				columnNameTypeMap.put(eachField, eachField.getType());
 			}
+
 		}
+
+		// to get the values generate a string of place holders
+		String insertSql = "INSERT INTO :tableName(:columns) VALUES(:values)".replaceAll(":tableName", this.table);
+
+		String columnNames = columnNameTypeMap.entrySet().stream().map(e -> e.getKey().getName())
+				.collect(Collectors.joining(","));
+		long nuOfCols = columnNameTypeMap.entrySet().stream().count();
+
+		String colValPlaceHolders = IntStream.range(0, (int) nuOfCols).mapToObj(e -> "?")
+				.collect(Collectors.joining(","));
+
+		System.out.println("Placeholders: " + colValPlaceHolders);
+		insertSql = insertSql.replaceAll(":columns", columnNames).replaceAll(":values", colValPlaceHolders);
+		System.out.println(insertSql);
+
 	}
 
 }
