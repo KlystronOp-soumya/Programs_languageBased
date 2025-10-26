3957c71ec0cf6bb620cc8dff2c52eac0a9ee307d
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-10-26 16:08:19 +0530

    adds DB classes

--- src/com/demo/annot/miniproj/Hibernate.java
+++ src/com/demo/annot/miniproj/Hibernate.java
@@ -3,7 +3,9 @@ package com.demo.annot.miniproj;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -11,6 +13,8 @@ public class Hibernate<T> {
 
 	private String table;
 	private Set<String> columns;
+
+	private AtomicInteger plId = new AtomicInteger(0);
 
 	public void save(List<T> entities) {
 
@@ -25,6 +29,7 @@ public class Hibernate<T> {
 			this.table = annotation.name().isBlank() ? clazz.getName() : annotation.name();
 			System.out.println("Provided table name: " + this.table);
 		}
+
 		/*
 		 * Annotation[] classAnnotations = clazz.getAnnotations(); for (Annotation
 		 * annotation : classAnnotations) { if
@@ -59,21 +64,27 @@ public class Hibernate<T> {
 		}
 
 		// to get the values generate a string of place holders
-		String insertSql = "INSERT INTO :tableName VALUES(:values)".replaceAll(":tableName", this.table);
+		String insertSql = "INSERT INTO :tableName(:columns) VALUES(:values)".replaceAll(":tableName", this.table);
 
-		/*
-		 * String columnNames = columnNameTypeMap.entrySet().stream().map(e ->
-		 * e.getKey().getName()) .collect(Collectors.joining(","));
-		 */
+		String columnNames = columnNameTypeMap.entrySet().stream().map(e -> e.getKey().getName())
+				.collect(Collectors.joining(","));
+
 		long nuOfCols = columnNameTypeMap.entrySet().stream().count();
 
 		String colValPlaceHolders = IntStream.range(0, (int) nuOfCols).mapToObj(e -> "?")
 				.collect(Collectors.joining(","));
 
 		System.out.println("Placeholders: " + colValPlaceHolders);
-		insertSql = insertSql/* .replaceAll(":columns", columnNames) */.replaceAll(":values", colValPlaceHolders);
+		insertSql = insertSql.replaceAll(":columns", columnNames).replaceAll(":values", colValPlaceHolders);
 		System.out.println(insertSql);
 
+		for (Entry<Field, Class<?>> en : columnNameTypeMap.entrySet()) {
+			// check the PK
+			en.getKey().setAccessible(true);
+			Object val = en.getKey().get(entity);
+			System.out.println("Name: " + en.getKey().getName() + " Val:" + en.getKey().get(entity));
+			// pstmt.setObject(i+1, val) ;
+		}
 	}
 
 }
