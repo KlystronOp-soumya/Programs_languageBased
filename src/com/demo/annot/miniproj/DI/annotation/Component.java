513a9b76222f748edea98f55c4998b343cc9ab05
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-10-26 16:07:30 +0530

    creats DI related annotations

---
+++ src/com/demo/annot/miniproj/DI/annotation/Component.java
@@ -0,0 +1,12 @@
+package com.demo.annot.miniproj.DI.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Component {
+
+}
